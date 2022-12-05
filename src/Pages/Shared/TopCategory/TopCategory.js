dd1ad0d13bb65db652c7256f23526ec942ebad92
Jahirul Islam <jahirul@elyranur.com>
2022-12-06 04:01:45 +0600

    navber added

--- src/Pages/Shared/TopCategory/TopCategory.js
+++ src/Pages/Shared/TopCategory/TopCategory.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom'
+
 
 const TopCategory = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect( () =>{
+        fetch('http://localhost:5000/course-categories')
+        .then( res => res.json())
+        .then( data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            <h2>Total Category</h2>
+            <h4>Total Category: {categories.length}</h4>
+            <div>
+                {
+                    categories.map( category => <p
+                    key={category.id}>
+                        <Link to={`/category/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -11,7 +11,7 @@ const Main = () => {
             <Header></Header>
             <Container>
             <Row>
-                <Col lg='3'>
+                <Col lg='3' className='d-none d-lg-block'>
                 <TopCategory></TopCategory>
                 </Col>
                 <Col lg='9'>
