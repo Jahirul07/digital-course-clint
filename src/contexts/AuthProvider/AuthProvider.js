60c88023fa7df3122d02e83aedf83bfc8555febd
Jahirul Islam <jahirul@elyranur.com>
2022-12-06 18:46:47 +0600

    login form added

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,16 +1,48 @@
-import React, {createContext} from 'react';
-// import {getAuth} from '../../firebase/firebase.config'
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+import app from '../../firebase/firebase.config';
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
+const auth = getAuth(app);
 
-// const auth = getAuth()
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+    const gitHubProvider = new GithubAuthProvider();
 
-const AuthProvider = ({children}) => {
-    const user = {displayName: 'Jahirul Islam'}
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
-    const authInfo = {user}
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
+    const loginWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+    const loginWithGitHub = () => {
+        return signInWithPopup(auth, gitHubProvider);
+    }
 
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log(currentUser)
+            setUser(currentUser)
+            setLoading(false)
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = { user, loading, logOut, createUser, signIn, loginWithGoogle, loginWithGitHub }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
